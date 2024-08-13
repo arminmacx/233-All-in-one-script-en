a506db11c4463e5a769e9bf4e05247fc2b2fd590
Armin <visualarmin@gmail.com>
2024-08-13 20:06:13 +0330

    Update dns.sh
    translate to english && add quad9 to dns list

--- src/dns.sh
+++ src/dns.sh
@@ -1,5 +1,6 @@
 is_dns_list=(
     1.1.1.1
+    9.9.9.9
     8.8.8.8
     h3://dns.google/dns-query
     h3://cloudflare-dns.com/dns-query
@@ -11,6 +12,9 @@ dns_set() {
     if [[ $1 ]]; then
         case ${1,,} in
         11 | 1111)
+            is_dns_use=${is_dns_list[0]}
+            ;;
+        99 | 9999)
             is_dns_use=${is_dns_list[0]}
             ;;
         88 | 8888)
@@ -29,21 +33,21 @@ dns_set() {
             if [[ $2 ]]; then
                 is_dns_use=${2,,}
             else
-                ask string is_dns_use "请输入 DNS: "
+                ask string is_dns_use "Please enter DNS: "
             fi
             ;;
         none)
             is_dns_use=none
             ;;
         *)
-            err "无法识别 DNS 参数: $@"
+            err "DNS parameters not recognized: $@"
             ;;
         esac
     else
         is_tmp_list=(${is_dns_list[@]})
-        ask list is_dns_use null "\n请选择 DNS:\n"
+        ask list is_dns_use null "\nPlease select DNS:\n"
         if [[ $is_dns_use == "set" ]]; then
-            ask string is_dns_use "请输入 DNS: "
+            ask string is_dns_use "Please enter DNS: "
         fi
     fi
     if [[ $is_dns_use == "none" ]]; then
@@ -52,5 +56,5 @@ dns_set() {
         cat <<<$(jq '.dns.servers=[{address:"'$is_dns_use'",address_resolver:"local"},{tag:"local",address:"local"}]' $is_config_json) >$is_config_json
     fi
     manage restart &
-    msg "\n已更新 DNS 为: $(_green $is_dns_use)\n"
+    msg "\nDNS has been updated to: $(_green $is_dns_use)\n"
 }
